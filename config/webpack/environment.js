6f9971b5ed28b7d93c2de9a2879a0d5ec52e8f2d
oscar ortiz <ortizg.oscar@gmail.com>
2018-03-30 14:58:47 -0300

    Peparing to deploy on heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,5 @@
 const { environment } = require('@rails/webpacker')
 // config/webpack/environment.js
-const { environment } = require('@rails/webpacker')
-
 // Bootstrap 3 has a dependency over jQuery:
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
